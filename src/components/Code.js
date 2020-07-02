6edadec7aadbf128a0e1d89c29427343046ae76f
Heechul Ryu <ryuhcii@gmail.com>
2020-07-02 19:40:21 +0100

    Detect language by file extension

--- src/components/Code.js
+++ src/components/Code.js
@@ -2,9 +2,9 @@ import React from 'react';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { a11yDark } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
-export const Code = ({children}) => (
+export const Code = ({children, lang}) => (
   <SyntaxHighlighter
-    language="javascript"
+    language={lang}
     style={a11yDark}>
     {children}
   </SyntaxHighlighter>
--- src/components/File.js
+++ src/components/File.js
@@ -2,6 +2,14 @@ import React from 'react';
 import { Box } from "@chakra-ui/core";
 import { Code } from './Code.js'
 
+// https://stackoverflow.com/a/680982/1570165
+const re = /(?:\.([^.]+))?$/;
+const filenameToLang = filename => {
+  const ext = re.exec(filename)[1];
+
+  return ext === undefined ? "text" : ext;
+};
+
 export const File = ({children, name}) => (
   <Box maxW="sm" borderWidth="1px" rounded="lg" overflow="hidden">
 
@@ -18,7 +26,10 @@ export const File = ({children, name}) => (
     </Box>
 
     <Box>
-      <Code px={2} py={2} mt={1} w="100%">
+      <Code
+        lang={filenameToLang(name)}
+        px={2} py={2} mt={1} w="100%"
+      >
         {children}
       </Code>
     </Box>
